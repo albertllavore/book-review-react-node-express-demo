2cae1ea3a68d4c35648d1234b616095a771ea8db
Albert Llavore <albertllavore@alberts-mbp.lan>
2020-07-29 17:23:22 -0700

    Add tests for Reviews api

--- README.md
+++ README.md
@@ -44,6 +44,8 @@ Open [http://localhost:8080](http://localhost:8080) to view it in the browser.
 The page will reload if you make edits.<br />
 You will also see any lint errors in the console.
 
+### `npm test`
+Runs the frontend and backend tests
 
 ## APIs
 
--- test/server.test.js
+++ test/server.test.js
@@ -2,12 +2,11 @@ const expect = require('chai').expect;
 const {jsdom} = require('jsdom');
 const request = require('supertest');
 const sqlite3 = require('sqlite3');
+let db = new sqlite3.Database('../database.sqlite');
 
 const app = require('../testserver');
 
 describe('/api/book routes', function() {
-  let db = new sqlite3.Database('../database.sqlite');
-
   describe('GET /api/book', function() {
 
     it('returns an array', function() {
@@ -88,160 +87,83 @@ describe('/api/book routes', function() {
     });
 
   });
-/*
-  describe('PUT /api/minions/:minionId', function() {
-    
-    it('updates the correct minion and returns it', function() {
-      let initialMinion;
-      let updatedMinionInfo;
+});
+
+describe('/api/review routes', function() {
+  describe('GET /api/review', function() {
+
+    it('returns an array', function() {
       return request(app)
-        .get('/api/minions/1')
+        .get('/api/review')
+        .expect(200)
         .then((response) => {
-          initialMinion = response.body
-        })
-        .then(() => {
-          updatedMinionInfo = Object.assign({}, initialMinion, {name: 'Test'});
-          return request(app)
-            .put('/api/minions/1')
-            .send(updatedMinionInfo);
-        })
-        .then((response) => {
-          expect(response.body).to.be.deep.equal(updatedMinionInfo);
+          expect(response.body).to.be.an.instanceOf(Array);
         });
     });
 
-    it('updates the correct minion and persists to the database', function() {
-      let initialMinion;
-      let updatedMinionInfo;
+    it('returns an array of all reviews', function() {
       return request(app)
-        .get('/api/minions/1')
+        .get('/api/review')
+        .expect(200)
         .then((response) => {
-          initialMinion = response.body
-        })
-        .then(() => {
-          updatedMinionInfo = Object.assign({}, initialMinion, {name: 'Persistence Test'});
-          return request(app)
-            .put('/api/minions/1')
-            .send(updatedMinionInfo);
-        })
-        .then(() => {
-          return request(app)
-            .get('/api/minions/1');
-        })
-        .then((response) => response.body)
-        .then(minionFromDatabase => {
-          expect(minionFromDatabase.name).to.equal('Persistence Test');
+          response.body.forEach((review) => {
+            expect(review).to.have.ownProperty('id');
+            expect(review).to.have.ownProperty('author');
+            expect(review).to.have.ownProperty('summary');
+            expect(review).to.have.ownProperty('review');
+            expect(review).to.have.ownProperty('rating');
+            expect(review).to.have.ownProperty('date');
+            expect(review).to.have.ownProperty('book_id');
+          });
         });
     });
+
+  });
+
+  describe('GET /review/:reviewId', function() {
+  
+      it('returns a single review object', function() {
+        return request(app)
+          .get(`/api/review/1`)
+          .expect(200)
+          .then((response) => {
+            const review = response.body;
+            expect(review).to.be.an.instanceOf(Object);
+            expect(review).to.not.be.an.instanceOf(Array);
+          });
+      });
+  
+      it('returns a full review object', function() {
+        return request(app)
+        .get(`/api/review/1`)
+        .expect(200)
+        .then((response) => {
+          let review = response.body;
+          expect(review).to.have.ownProperty('id');
+          expect(review).to.have.ownProperty('author');
+          expect(review).to.have.ownProperty('summary');
+          expect(review).to.have.ownProperty('review');
+          expect(review).to.have.ownProperty('rating');
+          expect(review).to.have.ownProperty('date');
+          expect(review).to.have.ownProperty('book_id');
+        });
+      });
+
+      // write test for bookId parameter
 
     it('called with a non-numeric ID returns a 404 error', function() {
       return request(app)
-        .put('/api/minions/notAnId')
+        .get('/api/review/notAnId')
         .expect(404);
     });
 
     it('called with an invalid ID returns a 404 error', function() {
       return request(app)
-        .put('/api/minions/450')
-        .expect(404);
-    });
-
-    it('called with an invalid ID does not change the database array', function() {
-      let initialMinionsArray;
-      return request(app)
-        .get('/api/minions')
-        .then((response) => {
-          initialMinionsArray = response.body;
-        })
-        .then(() => {
-          return request(app)
-            .put('/api/minions/notAnId')
-            .send({key: 'value'});
-        })
-        .then(() => {
-          return request(app).get('/api/minions');
-        })
-        .then((afterPutResponse) => {
-          let postRequestMinionsArray = afterPutResponse.body;
-          expect(initialMinionsArray).to.be.deep.equal(postRequestMinionsArray);
-        });
-    });
-
-  });
-
-  describe('POST /api/minions', function() {
-
-    it('should add a new minion if all supplied information is correct', function() {
-      let initialMinionsArray;
-      let newMinionObject = {
-        name: 'Test',
-        title: '',
-        salary: 0,
-        weaknesses: '',
-      }
-      return request(app)
-        .get('/api/minions')
-        .then((response) => {
-          initialMinionsArray = response.body;
-        })
-        .then(() => {
-          return request(app)
-            .post('/api/minions')
-            .send(newMinionObject)
-            .expect(201);
-        })
-        .then((response) => response.body)
-        .then((createdMinion) => {
-          newMinionObject.id = createdMinion.id;
-          expect(newMinionObject).to.be.deep.equal(createdMinion);
-        });
-    });
-
-  });
-
-  describe('DELETE /api/minions', function() {
-    
-    it('deletes the correct minion by id', function() {
-      let initialMinionsArray;
-      return request(app)
-        .get('/api/minions')
-        .then((response) => {
-          initialMinionsArray = response.body;
-        })
-        .then(() => {
-          return request(app)
-            .delete('/api/minions/1')
-            .expect(204);
-        })
-        .then(() => {
-          return request(app)
-            .get('/api/minions');
-        })
-        .then((response) => response.body)
-        .then((afterDeleteMinionsArray) => {
-          expect(afterDeleteMinionsArray).to.not.be.deep.equal(initialMinionsArray);
-          let shouldBeDeletedMinion = afterDeleteMinionsArray.find(el => el.id === '1');
-          expect(shouldBeDeletedMinion).to.be.undefined;
-        });
-
-    });
-
-    it('called with a non-numeric ID returns a 404 error', function() {
-      return request(app)
-        .delete('/api/minions/notAnId')
-        .expect(404);
-    });
-
-    it('called with an invalid ID returns a 404 error', function() {
-      return request(app)
-        .delete('/api/minions/450')
+        .get('/api/review/450')
         .expect(404);
     });
 
   });
-
-  */
-
 });
 
 /*
